b4866a1b971a52fb6a04119c00d87d70fd365724
Tanja Rosner <tr@pragma-shift.net>
2020-06-09 00:25:13 +0200

    wiederherstellung2

---
+++ Scorecards/scorecard.php
@@ -0,0 +1,29 @@
+<?php
+include 'class.scorecard.php';
+$scorecard = new scorecard();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reports Mockup</title>
+    <script src="../Jquery/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="scorecards_css.css">
+    <link rel="stylesheet" type="text/css" href="../columns/styles.css">
+    <link rel="stylesheet" type="text/css" href="../FontAwesome/css/all.css">
+</head>
+
+
+<body>
+<main>
+    <div class="content col-12">
+        <?php
+        echo $scorecard->show();
+        ?>
+    </div>
+</main>
+<script src="scorecards_js.js"></script>
+<script src="../columns/scripts.js"></script>
+</body>
+</html>
--- Scorecards/scorecards_js.js
+++ Scorecards/scorecards_js.js
@@ -45,22 +45,6 @@ $(document).ready(function() {
 
 
 
-    /* SEARCH TABLE SIDEBAR */
-
-    $('#tableSearchInputSSR').on('keyup', function () {
-        //get value of searchbar input
-        var value = $(this).val().toLowerCase();
-        //filter checkbox-labels for search value
-        $('.table-fields .checkboxes label').filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-        });
-        // when the title-part still has unfiltered checkboxes as siblings, show, otherwise hide
-        $('.table-fields .title-part').filter(function () {
-            $(this).toggle($(this).siblings().text().toLowerCase().indexOf(value) > -1);
-        });
-
-    });
-
 
     /* SELECT ALL CHECKBOXES FOR CURRENT GROUP */
 
@@ -74,103 +58,6 @@ $(document).ready(function() {
 
     /* TODO: IF ALL CHECKBOXES ARE SELECTED, CHECK "SELECT ALL" CHECKBOX */
 
-
-    /* ADD IMAGE-GALLERY-SIDEBAR FIELDS */
-
-    $('.add-gallery-fields').on('click', function () {
-        //get group-id of Sidebar-Fields
-        var id = $(this).attr('id');
-        //first-field with group-id as class will be cloned,
-        // cleaned and appended to container,
-        // also delete button will be added
-        $('.first-field .' + id).clone()
-            .find('input')
-            .val('')
-            .end()
-            .appendTo('#' + id + '~ .field-container')
-            .append('<i class=\'fas fa-times delete col-1\'></i>');
-    });
-    /* REMOVE IMAGE-GALLERY-SIDEBAR FIELDS*/
-    $('.field-container').on('click', '.delete', function () {
-        // remove sidebar-field and the delete button
-        $(this).siblings().remove();
-        $(this).remove();
-    });
-
-
-
-    /* TOGGLE BETWEEN SPECIFIC AND PERIOD TIMESPAN */
-
-    // initial status
-    if ($('input:checkbox#period-timespan').prop('checked')) {
-        //if checkbox for period timespan is checked,
-        // show period timespan and hide specific timespan
-        $('.period-timespan').show()
-        $('.specific-timespan').hide();
-    } else {
-        //if checkbox for period timespan is not checked,
-        // show specific timespan and hide period-timespan-fields
-        $('.period-timespan').hide();
-        $('.specific-timespan').show();
-    }
-
-    //on change
-    $('#period-timespan').on('change', function () {
-        //if checkbox for period timespan is checked,
-        // show period timespan and hide specific timespan,
-        // otherways hide
-        var checked = $('#period-timespan').prop('checked');
-        $('.specific-timespan').toggle(!checked);
-        $('.period-timespan').toggle(checked);
-    });
-
-
-
-
-
-
-    /* ADD CUSTOM FILTER FIELDS */
-
-    // variable for counting custom filter-fields
-    var filterid = 1;
-
-    $('.add-filter-fields').on('click', function () {
-        // on add, count variable up
-        filterid++;
-
-        //clone first-filter-dropdown,
-        // then clean the input-fields,
-        // clean the selects,
-        // add filter-count to classlist,
-        // and appednt it to container for new filter
-        // then remove the "first-filer" id
-        $('.dropdown#first-filter').clone()
-            .find('.dropdown-content input:text')
-            .val('')
-            .end()
-            .find('.dropdown-content select')
-            .prop('selectedIndex', 0)
-            .end()
-            .find('.dropdown-content')
-            .addClass('filter-' + filterid)
-            .removeClass('filter')
-            .end()
-            .appendTo('.new-filter')
-            .find('button.dropbtn')
-            .attr('id', 'filter-' + filterid)
-            .end()
-            .removeAttr('id');
-    });
-
-
-    /* REMOVE CUSTOM FILTER FIELDS*/
-
-    $('.new-filter').on('click', '.delete', function () {
-        //The highest parent div - the dropdown button has to be removed,
-        //trigger update-overlay after deleting a filter
-        $(this).parent().parent().parent().parent('div.dropdown').remove();
-        $('.dropdown-content').trigger('change');
-    });
 
 
     /* OVERLAY && UPDATE BUTTON*/
@@ -186,7 +73,23 @@ $(document).ready(function() {
     });
 
 
+
+    /* TABS */
+
+    $('.tabcontent').hide();
+
+
+    if ($('button.tablinks').hasClass('.active')){
+        var id = $(this).attr('id');
+        $('.tabcontent ' + id).show();
+    }
+
+
+    $('button.tablinks').on('click', function(){
+        var id = $(this).attr('id');
+
+        $('.tabcontent ' + id).show();
+    });
+
+
 });
-
-
-
