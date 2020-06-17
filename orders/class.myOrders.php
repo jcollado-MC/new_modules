f9486da7775e5e6b90bf1962daed2046bd3ce55b
Tanja Rosner <tr@pragma-shift.net>
2020-06-17 14:19:30 +0200

    loading data on init

--- orders/class.myOrders.php
+++ orders/class.myOrders.php
@@ -19,23 +19,26 @@ class myOrders extends Orders
         ini_set("memory_limit", "1024M");
         ini_set("max_execution_time", "1200");
 
-        // CHECK TYPE & LANGUAGE
+        $this -> products[1] = ['id' => 1,'product_id' => '', 'units' => '5', 'sap-number' => '', 'sap-name' => 'Gouda', 'discount1' => '', 'price' => '', 'tarif' => ''];
+        $this -> products[3] = ['id' => 3, 'product_id' => '', 'units' => '80', 'sap-number' => '', 'sap-name' => 'Mozzarella', 'discount1' => '', 'price' => '', 'tarif' => ''];
+        $this -> products[6] = ['id' => 6, 'product_id' => '', 'units' => '1337', 'sap-number' => '', 'sap-name' => 'Beef Burger', 'discount1' => '', 'price' => '', 'tarif' => ''];
+
 
 
         // LOAD FIELDS
-        $products[] = ['id' => 1, 'family' => 'Cheeses', 'name' => 'Gouda'];
-        $products[] = ['id' => 2, 'family' => 'Cheeses', 'name' => 'Gorgonzola'];
-        $products[] = ['id' => 3, 'family' => 'Cheeses', 'name' => 'Mozzarella'];
-        $products[] = ['id' => 4, 'family' => 'Cheeses', 'name' => 'Parmesan'];
-        $products[] = ['id' => 5, 'family' => 'Burgers', 'name' => 'Veggie Burger'];
-        $products[] = ['id' => 6, 'family' => 'Burgers', 'name' => 'Beef Burger'];
-        $products[] = ['id' => 7, 'family' => 'Burgers', 'name' => 'Cheese Burger'];
-        $products[] = ['id' => 8, 'family' => 'Burgers', 'name' => 'Chicken Burger'];
-        $products[] = ['id' => 9, 'family' => 'Burgers', 'name' => 'Pulled Pork Burger'];
-        $products[] = ['id' => 10, 'family' => 'Drinks', 'name' => 'Cola'];
-        $products[] = ['id' => 11, 'family' => 'Drinks', 'name' => 'Fanta'];
-        $products[] = ['id' => 12, 'family' => 'Drinks', 'name' => 'Sprite'];
-        $products[] = ['id' => 13, 'family' => 'Drinks', 'name' => 'Water'];
+        $products[1] = ['id' => 1, 'family' => 'Cheeses', 'name' => 'Gouda'];
+        $products[2] = ['id' => 2, 'family' => 'Cheeses', 'name' => 'Gorgonzola'];
+        $products[3] = ['id' => 3, 'family' => 'Cheeses', 'name' => 'Mozzarella'];
+        $products[4] = ['id' => 4, 'family' => 'Cheeses', 'name' => 'Parmesan'];
+        $products[5] = ['id' => 5, 'family' => 'Burgers', 'name' => 'Veggie Burger'];
+        $products[6] = ['id' => 6, 'family' => 'Burgers', 'name' => 'Beef Burger'];
+        $products[7] = ['id' => 7, 'family' => 'Burgers', 'name' => 'Cheese Burger'];
+        $products[8] = ['id' => 8, 'family' => 'Burgers', 'name' => 'Chicken Burger'];
+        $products[9] = ['id' => 9, 'family' => 'Burgers', 'name' => 'Pulled Pork Burger'];
+        $products[10] = ['id' => 10, 'family' => 'Drinks', 'name' => 'Cola'];
+        $products[11] = ['id' => 11, 'family' => 'Drinks', 'name' => 'Fanta'];
+        $products[12] = ['id' => 12, 'family' => 'Drinks', 'name' => 'Sprite'];
+        $products[13] = ['id' => 13, 'family' => 'Drinks', 'name' => 'Water'];
         foreach ($products as $row) {
             if ($row['family'] <> '') $group = $row['family'];
             $product = [];
@@ -45,6 +48,10 @@ class myOrders extends Orders
             $product['group'] = $row['family'];
             $this->groups[$group][] = $product;
         }
+
+
+
+
     }
 
 }
--- orders/class.orders.php
+++ orders/class.orders.php
@@ -47,11 +47,8 @@ class orders{
              
              
              $('.checkboxes input:checkbox').change(function(){
-                 var checked = $(this).prop('checked');
-                 
+                 var checked = $(this).prop('checked');                
                  $(this).parent().parent().find('.quantity').toggle(checked);
-
-                
              });
              
              
@@ -59,21 +56,57 @@ class orders{
                  
                  var idThis = $(this).attr('id');
                  var name = $(this).parent().text();
-                 var qty = 1;
-              
+                 var qty = $(this).parent().siblings().children('.quantity input').val();
+                 var length = table.rows.length;
                  
                  var checked = $(this).prop('checked');
                  if(checked){
-                     table.insertRow(  { id: '1', data:[ idThis, name, '' , '' , qty]} );
+                     table.insertRow( [idThis, name, '' , '' , qty] ,0, true);
                  } else {
-                     table.deleteRow();
-                 }
-                 
+                     for(let i = 0; i < length; i++){
+                         let val = table.getValueFromCoords([0], [i]);
+                         if(val == idThis){
+                             table.deleteRow(i);
+                         }
+                     }
+                 }                 
              });
              
+             $('.quantity input').change(function() {
+                 
+                 var idThis = $(this).parent().siblings().children('.checkboxes input:checkbox').attr('id');
+                 var qty = $(this).val();
+                 var length = table.rows.length;                
+                 
+                 for(let i = 0; i < length; i++){
+                         let val = table.getValueFromCoords([0], [i]);
+                         if(val == idThis){
+                             table.setValue( ('E' + (i+1)), qty, true);
+                         }
+                     }
+             });           
             
-            
+             
         });
+        
+        
+            var changed = function(instance, cell, x, y, value) {                
+                if (x == 4){
+                    console.log('value: ' + value);                    
+                    var id =  table.getValueFromCoords([0], [y]);    
+                    var quantityInput = $('#'+id).parent().siblings().children('.quantity input');                
+                    quantityInput.val(value);                
+                   
+//                   if( value <= 0 ) {                        
+//                        var checkbox = $('#'+id);                                                    
+//                        checkbox.prop('checked', false);                     
+//                        var quantity = $('#'+id).parent().siblings('.quantity'); 
+//                        quantity.hide();                       
+//                        table.deleteRow( y );
+//                    }
+               }
+                            
+            }
 
 
 
@@ -97,10 +130,6 @@ display: none;
 }
 
 </style>";
-
-
-
-
 
         $code .= "<script src='https://jexcel.net/v5/jexcel.js'></script>";
         $code .= "<script src='https://jexcel.net/v5/jsuites.js'></script>";
@@ -128,20 +157,14 @@ display: none;
 
 
     private function sidebar() {
-
-
         $cnt = 0;
-
         $code = "";
-
         $code .= "<div class='col-3'>";
         $code .= "<div class='col-12 tabs'>";
         $code .= "<h2>Order Settings</h2>";
-
         $code .= "<div class='search col-12'>
         <input class='col-11' type='text' id='orderSearchInput' placeholder='Search'>
-         <i class='fas fa-search search-icon col-1'></i>
-          
+         <i class='fas fa-search search-icon col-1'></i>          
     </div>";
         foreach($this->groups as $name => $products) {
             $code .= "<div class='title-part accordion' id='panel-".$cnt ." '>
@@ -152,12 +175,35 @@ display: none;
 
                 $code .= "<div class='panel-".$cnt." checkboxes'>";
                 foreach ($products as $product) {
-                    $code .= "   <div class='col-12 row'>
+                    $code .= "<div class='col-12 row'>
                         <label class='col-8 checkbox-label'>
-                            <input type='checkbox' id='". $product['id'] . "'>
-                            " . $product['name'] . "
-                        </label>
-                        <label class='col-4 quantity'>Quantity: <input  class='col-12' type='number' value='1' step='1'></label>
+                            <input type='checkbox' id='". $product['id'] ."'";
+                    if(isset($this -> products[$product['id']])){
+                        if ($this -> products[$product['id']]['units'] > 0) {
+                            $code .= "checked";
+                        }
+                    }
+                    $code .= ">";
+                    $code .= $product['name'];
+                    $code .= "</label>";
+                    $code .= "<label class='col-4 quantity'";
+                    if(isset($this -> products[$product['id']])){
+                        if ($this -> products[$product['id']]['units'] > 0) {
+                            $code .= "style='display: block' ";
+                        }
+                    }
+                    $code .= ">";
+                    $code .= "Quantity:";
+                    $code .= "<input  class='col-12' type='number' value='";
+                    if(isset($this -> products[$product['id']])){
+                        if ($this -> products[$product['id']]['units'] > 0) {
+                            $code .= $this->products[$product['id']]['units'];
+                        }
+                    } else{
+                        $code .= 1;
+                    }
+                    $code .= "' step='1'>";
+                    $code .= "</label>
                     </div>";
                 }
                 $code .= "</div>";
@@ -189,16 +235,65 @@ display: none;
       <th>PNF</th>
     </tr>
   </thead>
-<tbody>
-  
-   
+<tbody>";
+        foreach($this->groups as $name => $products) {
+            foreach ($products as $product) {
+                if (isset($this->products[$product['id']])) {
+                    if ($this->products[$product['id']]['units'] > 0) {
+                        $code .= "<tr>";
+
+                        $code .= "<td>";
+                        $code .= $this->products[$product['id']]['id'];
+                        $code .= "</td>";
+
+                        $code .= "<td>";
+                        $code .= $this->products[$product['id']]['sap-name'];
+                        $code .= "</td>";
+
+                        $code .= "<td>";
+                        $code .= "</td>";
+
+                        $code .= "<td>";
+                        $code .= "</td>";
+
+                        $code .= "<td>";
+                        $code .= $this->products[$product['id']]['units'];
+                        $code .= "</td>";
+
+                        $code .= "<td>";
+                        $code .= "</td>";
+
+                        $code .= "<td>";
+                        $code .= "</td>";
+
+                        $code .= "<td>";
+                        $code .= "</td>";
+
+
+                        $code .= "</tr>";
+                    }
+                }
+            }
+        }
+
+
+
+
+$code .= "<tr></tr>
+<tr></tr>
+<tr></tr>
+<tr></tr>
+<tr></tr>
 </tbody>
 </table>
     <script>
+    
+   
+    
     var table = jexcel(document.getElementById('spreadsheet'),
     {
     columns:[
-        {type:'text',width:'200px',readOnly:true},
+        {type:'text', title: 'ID',width:'200px',readOnly:true},
         {type:'text',width:'200px',readOnly:true},
         {type:'text',width:'100px',readOnly:true},
         {type:'text',width:'150px'},
@@ -207,8 +302,13 @@ display: none;
         {type:'text',width:'150px'},
         {type:'text',width:'150px'}
         ],
+        onchange: changed,
         license:'fd12c-f6d85-227f1-85ed4',
         });
+    
+    
+
+    
     
     </script>
     <hr>
