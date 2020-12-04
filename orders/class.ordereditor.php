eeb23430dc0f6b28ea9a2e949d6891693e0aea42
semihagursoy-mc <sguersoy@market-control.com>
2020-12-04 10:56:13 +0100

    unnecessary require deleted

--- orders/class.ordereditor.php
+++ orders/class.ordereditor.php
@@ -12,7 +12,7 @@
       header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
       exit("<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\r\n<html><head>\r\n<title>404 Not Found</title>\r\n</head><body>\r\n<h1>Not Found</h1>\r\n<p>The requested URL " . $_SERVER['SCRIPT_NAME'] . " was not found on this server.</p>\r\n</body></html>");
     }
-    require_once('../helpers.php');  //the files that you have the DB functions
+//the files that you have the DB functions require_once('../helpers.php');
     class OrderEditor{
         var $groups = []; //created this
         var $orders = []; // and this arrays here
